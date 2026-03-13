2eca8d07b9c6ef084c068d3c3ab58a73342647c4
josephchiao <chiaofeng.0118@gmail.com>
2026-03-13 00:32:14 -0700

    made it work lol

--- number_id_training_ex.py
+++ number_id_training_ex.py
@@ -42,19 +42,13 @@ def feed_NN_data(np_array):
     return np_array[:, 1:]/255, np.array([zeros[i] for i in np_array[:, 0]])
 
 def training_basic(np_array):
-    NN = nnw.NeuralNetwork((784,100,50,10), [nnw.ReLU, nnw.sigmoid, nnw.sigmoid, nnw.sigmoid], 'test_library/MNIST_CSV')
+    NN = nnw.NeuralNetwork((784,100,50,10), [nnw.ReLU, nnw.ReLU, nnw.sigmoid], 'test_library/MNIST_CSV')
     NN.theta_generate()
     NN.theta_recover()
     X, Y = feed_NN_data(np_array)
-    # number_display(np.reshape(X[1], (28,28)), Y[1])
-    # number_display(np.reshape(X[2], (28,28)), Y[2])
-    # number_display(np.reshape(X[3], (28,28)))
-    # print(np.shape(X[:100]), np.shape(Y[:100]))
-    # print(X[0])
-    for i in range(2):
+    for i in range(100):
         print(i)
-        NN.train(X[i*100:(i+1)*100], Y[i*100:(i+1)*100], 6000, 0.001)
-        # print(NN.b)
+        NN.train(X[i*100:(i+1)*100], Y[i*100:(i+1)*100], 1000, [0.01, 0.001][i >= 50])
     NN.theta_save()
 
 training_basic(load_training_example())
@@ -68,12 +62,18 @@ training_basic(load_training_example())
 # print(np.argmax(NN.feedforward(X[2])[-1]))
 # print(NN.feedforward(X[2])[-1])
 
-NN = nnw.NeuralNetwork((784,100,50,10), [nnw.ReLU, nnw.sigmoid, nnw.sigmoid, nnw.sigmoid], 'test_library/MNIST_CSV')
+NN = nnw.NeuralNetwork((784,100,50,10), [nnw.ReLU, nnw.ReLU, nnw.sigmoid], 'test_library/MNIST_CSV')
 NN.theta_recover()
-print(nnw.sigmoid(NN.b[-1]))
 X, Y = feed_NN_data(load_training_example(type = 'test'))
+Y_predict_mx = NN.feedforward(X)[-1]
+Y_predict = np.argmax(Y_predict_mx, axis = 1)
+correction_rate = sum([Y[i][Y_predict[i]] for i in range(len(Y_predict))])/len(Y)
+print('correction rate = ', correction_rate)
+
 for i in range(len(Y)):
     Y_predict_mx = NN.feedforward(X[i])[-1]
     Y_predict = np.argmax(Y_predict_mx)
-    print(Y_predict_mx)
     number_display(np.reshape(X[i], (28,28)), Y_predict)
+
+
+
